2df6ca3d4cffc81133de76ed38fb651e401c1319
Alex Robinson <alexsrobins@gmail.com>
2021-10-25 14:08:25 -0700

    Add files via upload

---
+++ lab_assignment_5.py
@@ -0,0 +1,28 @@
+"""
+Alexander Robinson
+Assignment 5 - Python 3A Foothill College 
+OCt 23, 2021
+"""
+
+
+
+def reverse_a_list(my_list):
+    """ reverse the order of my_list """
+    print(len(my_list))
+    length = len(my_list)
+    #  ... student code for base case goes here, may be more than one line ...
+    # ret_value =  ... student code for return value goes here and must include a call to reverse_a_list() ... 
+    return ret_value
+
+
+def main():
+    alphabet = [ letter for letter in "abcdefghijklmnopqrstuvwxyz" ]
+    print(alphabet)
+    print(reverse_a_list(alphabet))
+    print(alphabet)
+
+
+if __name__ == "__main__":
+    main()
+
+
